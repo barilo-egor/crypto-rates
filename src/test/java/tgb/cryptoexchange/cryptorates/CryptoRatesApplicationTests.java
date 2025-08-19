aef40eb501a603732168a0f817a65894ad6638d9
barilo <e.barilo@isida.by>
2025-08-19 16:00:10 +0300

    Initial commit

---
+++ src/test/java/tgb/cryptoexchange/cryptorates/CryptoRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.cryptorates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
