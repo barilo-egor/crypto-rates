db423f10004ec49807df40e999237682bfc34383
barilo <e.barilo@isida.by>
2025-08-20 18:17:43 +0300

    рефакторинг CoinGecko

--- src/main/java/tgb/cryptoexchange/cryptorates/service/exchange/CoinGeckoRateProvider.java
+++ src/main/java/tgb/cryptoexchange/cryptorates/service/exchange/CoinGeckoRateProvider.java
@@ -4,36 +4,50 @@ import org.springframework.stereotype.Service;
 import tgb.cryptoexchange.cryptorates.constants.CryptoPair;
 import tgb.cryptoexchange.cryptorates.constants.Exchange;
 import tgb.cryptoexchange.cryptorates.dto.CoinGeckoResponse;
+import tgb.cryptoexchange.cryptorates.exception.CryptoRatesException;
 import tgb.cryptoexchange.cryptorates.exception.UnsupportedCryptoPairException;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CoinGeckoRateProvider extends ExchangeRateProvider {
 
+    private final Map<CryptoPair, String> idsParams;
+
+    private final Map<CryptoPair, String> vsCurrenciesParams;
+
     protected CoinGeckoRateProvider(ExchangeWebClientFactory exchangeWebClientFactory) {
         super(exchangeWebClientFactory);
+        this.idsParams = new HashMap<>();
+        this.idsParams.put(CryptoPair.BTC_USD, "bitcoin");
+        this.idsParams.put(CryptoPair.LTC_USD, "litecoin");
+        this.idsParams.put(CryptoPair.BTC_RUB, "bitcoin");
+        this.idsParams.put(CryptoPair.LTC_RUB, "litecoin");
+        this.vsCurrenciesParams = new HashMap<>();
+        this.vsCurrenciesParams.put(CryptoPair.BTC_USD, "usd");
+        this.vsCurrenciesParams.put(CryptoPair.LTC_USD, "usd");
+        this.vsCurrenciesParams.put(CryptoPair.BTC_RUB, "rub");
+        this.vsCurrenciesParams.put(CryptoPair.LTC_RUB, "rub");
+        if (!getExchange().getPairs().stream().allMatch(idsParams::containsKey)
+                || !getExchange().getPairs().stream().allMatch(vsCurrenciesParams::containsKey)) {
+            throw new CryptoRatesException("Some CoinGecko crypto pair does not have params.");
+        }
     }
 
     @Override
     public BigDecimal getRate(CryptoPair cryptoPair) {
-        String idsParam = switch (cryptoPair) {
-            case BTC_USD, BTC_RUB -> "bitcoin";
-            case LTC_USD, LTC_RUB -> "litecoin";
-            default -> throw new UnsupportedCryptoPairException("Unsupported crypto pair");
-        };
-        String vsCurrenciesParam = switch (cryptoPair) {
-            case BTC_USD, LTC_USD -> "usd";
-            case BTC_RUB, LTC_RUB -> "rub";
-            default -> throw new UnsupportedCryptoPairException("Unsupported crypto pair");
-        };
-        final String finalIdsParam = idsParam;
-        final String finalVsCurrenciesParam = vsCurrenciesParam;
+        if (!getExchange().getPairs().contains(cryptoPair)) {
+            throw new UnsupportedCryptoPairException("Unsupported crypto pair: " + cryptoPair);
+        }
+        String idsParam = idsParams.get(cryptoPair);
+        String vsCurrenciesParam = vsCurrenciesParams.get(cryptoPair);
         CoinGeckoResponse response = exchangeWebClientFactory.get(
                 Exchange.COIN_GECKO,
                 uriBuilder -> uriBuilder.path("/simple/price")
-                        .queryParam("ids", finalIdsParam)
-                        .queryParam("vsCurrencies", finalVsCurrenciesParam)
+                        .queryParam("ids", idsParam)
+                        .queryParam("vsCurrencies", vsCurrenciesParam)
                         .build(),
                 CoinGeckoResponse.class
         );
