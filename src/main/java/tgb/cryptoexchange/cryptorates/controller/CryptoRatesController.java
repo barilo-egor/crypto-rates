1fa27269adb1e7ec46715b8580fc5d0392be4736
barilo <e.barilo@isida.by>
2025-08-20 10:55:45 +0300

    удален RateProvider и его реализация, получение курса завязано на клиентов бирж и их валютные пары

--- src/main/java/tgb/cryptoexchange/cryptorates/controller/CryptoRatesController.java
+++ src/main/java/tgb/cryptoexchange/cryptorates/controller/CryptoRatesController.java
@@ -1,19 +1,31 @@
 package tgb.cryptoexchange.cryptorates.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tgb.cryptoexchange.cryptorates.constants.CryptoPair;
 import tgb.cryptoexchange.cryptorates.dto.CryptoRate;
+import tgb.cryptoexchange.cryptorates.service.rate.RateService;
 import tgb.cryptoexchange.web.ApiResponse;
 
 @RestController
 @RequestMapping("/crypto-rates")
 public class CryptoRatesController {
 
+    private final RateService rateService;
+
+    public CryptoRatesController(RateService rateService) {
+        this.rateService = rateService;
+    }
+
     @GetMapping("/{pair}")
-    public ApiResponse<CryptoRate> getCryptoRate(@PathVariable CryptoPair pair) {
-        return null;
+    public ResponseEntity<ApiResponse<CryptoRate>> getCryptoRate(@PathVariable CryptoPair pair) {
+        return new ResponseEntity<>(
+                ApiResponse.success(rateService.getRate(pair)),
+                HttpStatus.OK
+        );
     }
 }
--- src/main/java/tgb/cryptoexchange/cryptorates/service/rate/RateService.java
+++ src/main/java/tgb/cryptoexchange/cryptorates/service/rate/RateService.java
@@ -1,33 +1,52 @@
 package tgb.cryptoexchange.cryptorates.service.rate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tgb.cryptoexchange.cryptorates.constants.CryptoPair;
 import tgb.cryptoexchange.cryptorates.dto.CryptoRate;
 import tgb.cryptoexchange.cryptorates.exception.CryptoRatesException;
+import tgb.cryptoexchange.cryptorates.service.exchange.ExchangeClient;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
 
 @Service
+@Slf4j
 public class RateService {
 
-    private final Map<CryptoPair, RateProvider> rateProviders;
+    private final Map<CryptoPair, List<ExchangeClient>> exchangeClients;
 
-    public RateService(List<RateProvider> rateProviders) {
-        this.rateProviders = new HashMap<>();
-        for (RateProvider rateProvider : rateProviders) {
-            this.rateProviders.put(rateProvider.getCryptoPair(), rateProvider);
+    public RateService(List<ExchangeClient> exchangeClients) {
+        this.exchangeClients = new HashMap<>();
+        for (ExchangeClient exchangeClient : exchangeClients) {
+            for (CryptoPair cryptoPair : exchangeClient.getExchange().getPairs()) {
+                this.exchangeClients.computeIfAbsent(cryptoPair, k -> new ArrayList<>()).add(exchangeClient);
+            }
         }
     }
 
     public CryptoRate getRate(CryptoPair cryptoPair) {
-        RateProvider rateProvider = rateProviders.get(cryptoPair);
-        if (Objects.isNull(rateProvider)) {
-            throw new CryptoRatesException("No provider found for " + cryptoPair);
+        List<ExchangeClient> exchangeClients = this.exchangeClients.get(cryptoPair);
+        if (exchangeClients == null || exchangeClients.isEmpty()) {
+            throw new CryptoRatesException("No exchange clients found for " + cryptoPair);
         }
-        return rateProvider.provide();
+        for (ExchangeClient exchangeClient : exchangeClients) {
+            try {
+                BigDecimal rate = exchangeClient.getRate(cryptoPair);
+                if (Objects.nonNull(rate)) {
+                    CryptoRate cryptoRate = new CryptoRate();
+                    cryptoRate.setRate(rate.setScale(8, RoundingMode.HALF_UP).doubleValue());
+                    cryptoRate.setPair(cryptoPair);
+                    cryptoRate.setTimestamp(System.currentTimeMillis());
+                    return cryptoRate;
+                }
+            } catch (Exception e) {
+                log.warn("Ошибка при получении курса валютной пары {} у биржи {}:",
+                        cryptoPair.name(), exchangeClient.getExchange().name(), e);
+            }
+        }
+        throw new CryptoRatesException("Couldn't get a rate from any exchange for " + cryptoPair.name());
     }
 
 }
