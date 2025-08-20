561d3619a5f5393ed40e0e081522d94fdc41aaaa
barilo <e.barilo@isida.by>
2025-08-20 16:57:19 +0300

    BinanceRateProviderTest

---
+++ src/test/java/tgb/cryptoexchange/cryptorates/service/exchange/BinanceRateProviderTest.java
@@ -0,0 +1,77 @@
+package tgb.cryptoexchange.cryptorates.service.exchange;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import tgb.cryptoexchange.cryptorates.constants.CryptoPair;
+import tgb.cryptoexchange.cryptorates.constants.Exchange;
+import tgb.cryptoexchange.cryptorates.dto.BinanceResponse;
+import tgb.cryptoexchange.cryptorates.exception.UnsupportedCryptoPairException;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BinanceRateProviderTest {
+
+    @Mock
+    private ExchangeWebClientFactory exchangeWebClientFactory;
+
+    @InjectMocks
+    private BinanceRateProvider binanceRateProvider;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"USD_RUB", "XMR_USD"})
+    @DisplayName("getRate(CryptoPair cryptoPair) - неподдерживаемая валютная пара - проброс UnsupportedCryptoPairException")
+    void shouldThrowUnsupportedCryptoPairException(CryptoPair cryptoPair) {
+        assertThrows(
+                UnsupportedCryptoPairException.class,
+                () -> binanceRateProvider.getRate(cryptoPair),
+                "Unsupported crypto pair"
+        );
+    }
+
+    static List<CryptoPair> getCryptoPairs() {
+        return Exchange.BINANCE.getPairs();
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCryptoPairs")
+    void shouldReturnNullIfResponseIsNull(CryptoPair cryptoPair) {
+        when(exchangeWebClientFactory.get(eq(Exchange.BINANCE), any(), eq(BinanceResponse.class))).thenReturn(null);
+        BigDecimal actual = binanceRateProvider.getRate(cryptoPair);
+        assertNull(actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCryptoPairs")
+    void shouldReturnRateForAllCryptoPairs(CryptoPair cryptoPair) {
+        BinanceResponse binanceResponse = new BinanceResponse();
+        binanceResponse.setPrice(BigDecimal.ONE);
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Function<UriBuilder, URI>> captor =
+                (ArgumentCaptor<Function<UriBuilder, URI>>) (ArgumentCaptor<?>) ArgumentCaptor.forClass(Function.class);
+        when(exchangeWebClientFactory.get(eq(Exchange.BINANCE), captor.capture(), eq(BinanceResponse.class))).thenReturn(binanceResponse);
+        BigDecimal actual = binanceRateProvider.getRate(cryptoPair);
+        Function<UriBuilder, URI> value = captor.getValue();
+        URI result = value.apply(new DefaultUriBuilderFactory().builder());
+        assertEquals("/avgPrice", result.getPath());
+        assertTrue(result.getQuery().startsWith("symbol="));
+        assertEquals(binanceResponse.getPrice(), actual);
+    }
+}
