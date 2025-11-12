675ff3cc5eb77e2cb62a8fca674b7c022f4bd704
barilo <e.barilo@isida.by>
2025-11-12 13:08:20 +0300

    swagger документация

--- src/main/java/tgb/cryptoexchange/cryptorates/controller/CryptoRatesController.java
+++ src/main/java/tgb/cryptoexchange/cryptorates/controller/CryptoRatesController.java
@@ -1,5 +1,9 @@
 package tgb.cryptoexchange.cryptorates.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tgb.cryptoexchange.controller.ApiController;
 import tgb.cryptoexchange.cryptorates.constants.CryptoPair;
 import tgb.cryptoexchange.cryptorates.dto.CryptoRate;
 import tgb.cryptoexchange.cryptorates.service.rate.RateService;
@@ -17,7 +22,7 @@ import tgb.cryptoexchange.web.LogResponseBody;
 @RestController
 @RequestMapping("/crypto-rates")
 @LogResponseBody
-public class CryptoRatesController {
+public class CryptoRatesController extends ApiController {
 
     private final RateService rateService;
 
@@ -25,6 +30,17 @@ public class CryptoRatesController {
         this.rateService = rateService;
     }
 
+    @Operation(
+            summary = "Получение курса криптовалютной пары.",
+            description = "Получение текущего курса любой из доступных бирж по валютной паре."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Курс получен."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера.",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
     @GetMapping("/{pair}")
     public ResponseEntity<ApiResponse<CryptoRate>> getCryptoRate(@PathVariable CryptoPair pair) {
         log.trace("Запрос на получение курса для пары {}", pair.name());
